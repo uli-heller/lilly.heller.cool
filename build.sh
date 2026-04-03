c0d49e8745f292f78ec2770b9809b9bbdd14d375
Uli Heller <uli.heller@daemons-point.com>
2026-04-03 17:26:45 +0200

    Drittes Rätsel

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 pandoc --standalone index.md >index.html
 pandoc --standalone 01.md >01.html
 pandoc --standalone 4.md  >4.html
+pandoc --standalone 240.md  >240.html
+pandoc --standalone 3.md  >3.html
--- index.md
+++ index.md
@@ -2,4 +2,4 @@
 
 <a href="01.html?a=1"><img src="cat.png" width="600"></img></a>
 
-Drücke auf das Katzenohr!
+Drücke auf das Hasenohr!
